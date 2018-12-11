b203f2ca52fa7e2d0c8405ffe5848ed40316dce4
Dan <shenanigans2952@gmail.com>
2018-12-10 22:31:12 -0500

    [DS] Created config for future scrapers

--- src/controllers/scraperController.js
+++ src/controllers/scraperController.js
@@ -1,15 +1,17 @@
-const scraper = require("../scrapers/scraper");
+const scrapers = require("../scrapers/scraper");
+const urlBuilder = require("../utils/urlBuilder");
+const websiteConfig = require("../config/websites.json");
 
 function scrapeUrls(request, response, next) {
-    const urls = [
-        "https://www.reddit.com",
-        "https://www.reddit.com/r/leagueoflegends"
-    ]
     // const { urls } = request.body || {};
-
-    scraper.redditScraper.scrapeUrls(urls).then(data => {
-        response.json(data);
-    }).catch(error => next(error))
+    Object.entries(websiteConfig).forEach(([hostname, config]) => {
+        const { sites, scraperName } = config;
+        const urls = sites.map(urlBuilder.bind(null, hostname));
+    
+        scrapers[scraperName].scrapeUrls(urls).then(data => {
+            response.json(data);
+        }).catch(error => next(error))
+    })
 }
 
 module.exports = {
